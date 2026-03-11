b90df97f93b8415617f983423d211d506527727d
Kevin <kjm520@gmail.com>
2026-03-11 15:16:13 -0500

    Comments for context and clarity of purpose

--- scripts/CLPR/email.js
+++ scripts/CLPR/email.js
@@ -1,9 +1,16 @@
 function sendEmail(to, subject, body) {
 
+    // Counter to the nonstop spam I get asking about SEO or if I want to see their price list about mistakes on my website.
+
+    // seriously if somehow you are reading this and are in any way associated with ANY operation that spams emails, GTFO.
+
+
+
+    // trigger, every 5 min, 
     GmailApp.sendEmail(
         'Sandra.sheh@outlook.com',
         'Re: Web Design Proposal',
-        'I would like to see the price list and packages for your WEB DESIGN service.'
+        'Yes, I would like to see the price list and packages for your WEB DESIGN service.'
     );
 
 }
